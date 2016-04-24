96bbdf6fa907ee7d7d512e3a3ea04919820dc90c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-04-24 09:21:06 -0600

    Finally got things updating; going to be working from the python server for a bit.

--- js/code.js
+++ js/code.js
@@ -1,6 +1,13 @@
 var count = 0;
 var date;
 
+var months = [ "January",  "February", "March",  "April",     "May", 
+               "June",     "July",     "August", "September", "October",
+               "November", "December" ];
+
+var days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+             "Saturday" ];
+
 function updateGreeting() {
     var greetingStr = "Hello, Max.";
 
@@ -16,13 +23,65 @@ function updateGreeting() {
     $('#greeting').html(greetingStr);
 }
 
-function updateDate() {
-    var dateStr = "Today is "+date.toLocaleFormat("%A, %B %d %Y.")
-    $('#date').html(dateStr);
+function getDateString() {
+    var dateStr = "Today is ";
+
+    dateStr += days[date.getDay()];
+    dateStr += ", ";
+    dateStr += months[date.getMonth()];
+    dateStr += " ";
+
+    var day = date.getDate();
+    var suffix = "th";
+    if(day != 11 && day != 12 && day != 13) {
+        if(day % 10 == 1) {
+            suffix = "st";
+        } else if(day % 10 == 2) {
+            suffix = "nd";
+        } else if(day % 10 == 3) {
+            suffix = "rd";
+        }
+    }
+    dateStr += day + suffix;
+
+    dateStr += ", ";
+    dateStr += date.getFullYear();
+    dateStr += ".";
+
+    return dateStr;
+}
+
+function getTimeString() {
+    var timeStr = "It is ";
+
+    var hour = date.getHours();
+    var half = "AM";
+    if(hour > 12) {
+        hour -= 12;
+        half = "PM";
+    }
+
+    timeStr += hour;
 
     var colon = ":";
-    if(date.getSeconds() % 2 == 0) { colon = " "; }
-    var timeStr = "It is "+date.toLocaleFormat("%l"+colon+"%M %p.");
+    if(date.getSeconds() % 2 == 1) {
+        var colon = " ";
+    }
+    timeStr += colon;
+
+    timeStr += date.getMinutes();
+    timeStr += " ";
+    timeStr += half;
+    timeStr += ".";
+
+    return timeStr;
+}
+
+function updateDate() {
+    var dateStr = getDateString();
+    $('#date').html(dateStr);
+
+    var timeStr = getTimeString();
     $('#time').html(timeStr);
 }
 
@@ -58,7 +117,6 @@ function tick() {
 function setup() {
     date = new Date();
 
-    updateDate();
     updateGreeting();
     updateDate();
     updateWeather();
